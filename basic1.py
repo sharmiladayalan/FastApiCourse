eb488b44b3c35b598b75dbfc984e440571529446
DESKTOP-DVE5ASN\ADMIN <sharmiladayalan18@gmail.com>
2026-01-11 17:50:01 +0530

    Put,Patch,Delete Method

--- basic1.py
+++ basic1.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, status, HTTPException
-from typing import Any
+from typing import Any,Optional
 
 app = FastAPI()
 
@@ -121,3 +121,103 @@ def get_shipments(id: int):
             detail = "requested id doesn't exist"
         )
     return shipments[id]
+
+
+'''
+===============
+Put Method
+===============
+'''
+
+#getting an user Id from user and updating the shipment status
+@app.put("/shipment")
+def add_data(id: int, weight: float, content: str, shipment_status: str):
+    if id in shipments:
+        raise HTTPException(
+            status_code = status.HTTP_400_BAD_REQUEST,
+            detail = "Shipment ID already exists"
+        )
+    else:
+        shipments[id] ={
+            "weight" : weight,
+            "content" : content,
+            "status" : shipment_status }
+        return {"message": f"shipment {shipments[id]} added successfully"}
+    
+#Now auto increment the shipment ID by using previous id (usinf max function)
+@app.put("/shipment_auto")
+def add_data_auto(content: str, weight: float, shipment_status: str):
+    #Validate Weight
+    if weight >= 25:
+        raise(HTTPException(
+            status_code = status.HTTP_406_NOT_ACCEPTABLE,
+            detail= "Weight should not be more than 25Kg"
+        ))
+    new_id = max(shipments.keys()) + 1
+    shipments[new_id] = {
+        "weight": weight,
+        "content": content,
+        "status": shipment_status
+    }
+    return {"message": f"Shipment with ID {new_id} added successfully."}
+
+@app.put("/shipment_update/{id}")
+def update_shipment(id: int, weight: float, content: str, shipment_status: str):
+    shipments[id] = {
+        "weight": weight,
+        "content": content,
+        "status": shipment_status
+    }
+    return shipments[id]
+
+#By using this post we can update insert or update entire date but we cannot update single field 
+#For this we use patch method
+'''
+=============
+Patch Method
+=============
+ '''
+@app.patch("/shipment_update/{id}")
+def patch_shipment(
+    id: int,
+    content: Optional[str] = None,
+    weight: Optional[float] = None,
+    status: Optional[str] = None
+):
+    if id not in shipments:
+        raise HTTPException(status_code=404, detail="Shipment not found")
+
+    shipment = shipments[id]
+    #Now shipment local variable contains the data of shipments[id] 
+    #For example 1223:{"weight" : 5.8,"content" : "Wooden table","status"  : "In transit"}
+
+    if content is not None:
+        shipment["content"] = content
+    if weight is not None:
+        shipment["weight"] = weight
+    if status is not None:
+        shipment["status"] = status
+
+    shipments[id] = shipment
+    #Now we update the data in shipment local variable to shipments dictionary
+    return shipment
+
+@app.patch("/shipment")
+def patch_shipment_1(id: int, body: dict[str, Any]):
+    shipment = shipments[id]
+    shipment.update(body)
+    shipments[id] = shipment
+    return shipment
+
+'''
+============
+Delete
+============
+'''
+@app.delete("/shipment/{id}")
+def delete_shipment(id: int) -> dict[str, Any]:
+    if id not in shipments:
+        raise HTTPException(status_code=404, detail="Shipment ID not Found")
+    del shipments[id]
+    # shipments.pop(id)
+    return {"Message": f'Shipment ID {id} deleted successfully'}
